11b242484b18c4772ebee5632814b327851c389f
unknown <45177460+akalikadien@users.noreply.github.com>
2021-01-01 21:36:36 +0100

    added navbar and some css styling

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*- 
 # __author__: Adarsh Kalikadien #
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
+
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/test1")
+def test1():
     # establish database connection and make cursor
     try:
         conn = sqlite3.connect('test.db')
@@ -18,12 +29,7 @@ def home():
     c.execute("SELECT * FROM study_locations WHERE test_column_3 = 'test_data_3'")
     data = c.fetchall()
     column_names = [description[0] for description in c.description]
-    return render_template("home.html", data=data, column_names=column_names)
-
-
-@app.route("/test")
-def salvador():
-    return "Hello, this is a test!"
+    return render_template("test1.html", data=data, column_names=column_names)
 
 
 if __name__ == "__main__":
