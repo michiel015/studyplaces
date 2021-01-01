11c4d9919e30154cfea9cb3ee20c705ff01cc317
unknown <45177460+akalikadien@users.noreply.github.com>
2021-01-01 23:36:44 +0100

    added test table, modified styles and main, updated todo

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 Setup: install python 3.5> from the [Python homepage](https://www.python.org/downloads/).
 Afterwards run:  
 `pip install -r requirements.txt` in the repo folder.  
-Website logic is contained in main.py, by running this file you can view the website at http://127.0.0.1:5000/ by default. 
+Website logic is contained in main.py, by running this file you can view the website at http://127.0.0.1:5000/ by default.  
+  
+**Todo**  
+* Fix layout and variable naming
+* Make jinja html template and use inheritance for other html files
+* Pass correct variables per city to html cards
--- main.py
+++ main.py
@@ -8,8 +8,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-
-    return render_template("home.html")
+    # establish database connection and make cursor
+    try:
+        conn = sqlite3.connect('test.db')
+    except:
+        print('could not establish connection to db')
+        exit(1)
+    c = conn.cursor()
+    c.execute("SELECT * FROM study_locations WHERE test_column_3 = 'test_data_3'")
+    data = c.fetchall()
+    column_names = [description[0] for description in c.description]
+    return render_template("home.html", data=data, column_names=column_names)
 
 
 @app.route("/about")
