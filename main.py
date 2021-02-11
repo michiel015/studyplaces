e161c5ff532ac2f55f135f8aae131d512bfa8500
unknown <45177460+akalikadien@users.noreply.github.com>
2021-02-11 22:32:23 +0100

    modified data extraction, added test csv, modified base html for home, modified home.html jinja to enable displaying multiple locations

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # __author__: Adarsh Kalikadien #
 import sqlite3
 from flask import Flask, render_template, url_for
-
+from utilities import *
 app = Flask(__name__)
 
 
@@ -15,11 +15,23 @@ def home():
         print('could not establish connection to db')
         exit(1)
     c = conn.cursor()
-    # c.execute("SELECT * FROM study_locations WHERE test_column_3 = 'test_data_3'")
-    c.execute("SELECT * FROM study_locations")
-    data = c.fetchall()
+    c.execute("select * from study_locations")
     column_names = [description[0] for description in c.description]
-    return render_template("home.html", data=data, column_names=column_names)
+    # c.execute("SELECT * FROM study_locations WHERE test_column_3 = 'test_data_3'")
+    conn.row_factory = dict_factory
+    c.execute("select * from study_locations")
+    data = c.fetchall()
+    c.execute("SELECT study_location FROM study_locations")
+    # study_locations_tuples is a list of tuples, so for the location we only need the first element of each tuple
+    study_locations_tuples = c.fetchall()
+    study_locations_list = [study_locations_tuple[0] for study_locations_tuple in study_locations_tuples]
+
+    # old query in which a single location is fetched
+    # c.execute("SELECT total_score, price_consumptions_norm, access_hours_norm, google_review FROM study_locations "
+    #           "where city = 'Delft'")
+    # data = c.fetchall()
+
+    return render_template("home.html", study_locations=study_locations_list, data=data, column_names=column_names)
 
 
 @app.route("/about")
--- utilities.py
+++ utilities.py
@@ -13,10 +13,22 @@ def convert_csv_to_sqlite_db(csv_filename):
     '''
     db_filename = csv_filename.split('.')[0]
     conn = sqlite3.connect(db_filename + '.db')
-    location_csv = pd.read_csv('test.csv')  # load in dataframe
+    location_csv = pd.read_csv(csv_filename)  # load in dataframe
     # create table study_locations in test.db
     location_csv.to_sql('study_locations', conn, if_exists='replace', index=False)
 
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 if __name__ == "__main__":
-    convert_csv_to_sqlite_db('test.csv')
+    # convert_csv_to_sqlite_db('test.csv')
+    con = sqlite3.connect("test.db")
+    con.row_factory = dict_factory
+    cur = con.cursor()
+    cur.execute("select * from study_locations")
+    print(cur.fetchall()[0]["study_location"])
