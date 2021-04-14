a3cbb3f74d9f0ea8dfb4648bcbb311ede76086a2
unknown <45177460+akalikadien@users.noreply.github.com>
2021-04-14 23:01:25 +0200

    updated dynamic pages urls, added extra criteria to dataset

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ def home():
         print('could not establish connection to db')
         exit(1)
     c = conn.cursor()
-    c.execute("select * from study_locations ORDER BY totaal DESC;")
+    c.execute("select * from study_locations ORDER BY totale_score DESC;")
     column_names = [description[0] for description in c.description]
     # c.execute("SELECT * FROM study_locations WHERE test_column_3 = 'test_data_3'")
     conn.row_factory = dict_factory
-    c.execute("select * from study_locations ORDER BY totaal DESC;")
+    c.execute("select * from study_locations ORDER BY totale_score DESC;")
     data = c.fetchall()
-    c.execute("SELECT werkplek FROM study_locations ORDER BY totaal DESC;")
+    c.execute("SELECT werkplek FROM study_locations ORDER BY totale_score DESC;")
     # study_locations_tuples is a list of tuples, so for the location we only need the first element of each tuple
     study_locations_tuples = c.fetchall()
     study_locations_list = [study_locations_tuple[0] for study_locations_tuple in study_locations_tuples]
@@ -53,23 +53,23 @@ def location_page(variable):
         print('could not establish connection to db')
         exit(1)
     c = conn.cursor()
-    c.execute("select * from study_locations ORDER BY totaal DESC;")
+    c.execute("select * from study_locations ORDER BY totale_score DESC;")
     column_names = [description[0] for description in c.description]
     # c.execute("SELECT * FROM study_locations WHERE test_column_3 = 'test_data_3'")
     conn.row_factory = dict_factory
-    c.execute("select * from study_locations ORDER BY totaal DESC;")
+    c.execute("select * from study_locations ORDER BY totale_score DESC;")
     data = c.fetchall()
-    c.execute("SELECT werkplek FROM study_locations ORDER BY totaal DESC;")
+    c.execute("SELECT url_name FROM study_locations ORDER BY totale_score DESC;")
     # study_locations_tuples is a list of tuples, so for the location we only need the first element of each tuple
     study_locations_tuples = c.fetchall()
-    study_locations_list = [study_locations_tuple[0] for study_locations_tuple in study_locations_tuples]
+    study_locations_list = [study_locations_tuple[-1] for study_locations_tuple in study_locations_tuples]
 
     # old query in which a single location is fetched
     # c.execute("SELECT total_score, price_consumptions_norm, access_hours_norm, google_review FROM study_locations "
     #           "where city = 'Delft'")
     # data = c.fetchall()
     if variable in study_locations_list:
-        c.execute("SELECT * FROM study_locations WHERE werkplek = '{idf}'".format(idf=variable))
+        c.execute("SELECT * FROM study_locations WHERE url_name = '{idf}'".format(idf=variable))
         data1 = c.fetchall()
         # data1 = c.execute("SELECT werkplek FROM study_locations WHERE werkplek = 'TU Delft Bibliotheek'")
     return render_template("modal.html", study_locations=variable, data=data1, column_names=column_names)
