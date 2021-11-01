bb0402293a2e0c0a04d963d6385dca6707062c42
michiel015 <michielkouwenhoven015@gmail.com>
2021-11-01 18:30:53 +0100

    fixed & changed meta description, and updated sitemap, added amsterdam picture

--- main.py
+++ main.py
@@ -42,10 +42,10 @@ def home():
     # c.execute("SELECT total_score, price_consumptions_norm, access_hours_norm, google_review FROM study_locations "
     #           "where city = 'Delft'")
     # data = c.fetchall()
-    explanation_title = "Werkplekwijzer - Vind de beste werkplek bij jou in de buurt"
+    explanation_title = "Werkplekwijzer - Vind eenvoudig de beste werkplek bij jou in de buurt"
     explanation_body = "Een goede werkplek vinden buiten kantoor kan een uitdaging zijn. Werkplekwijzer beoordeelt werkplekken in Nederland op verschillende criteria zodat jij de beste werkplek kunt vinden. Wij maken scoreboards en delen ervaringen over werkplekken in jouw omgeving. Hiermee kan je op werkplekwijzer de beste keuze maken voor een werkplek buiten kantoor. Of je nu op zoek bent naar een café met de lekkerste koffie, of een werkplek met het snelste internet. Alle beoordelingen per werkplek zijn eenvoudig te vinden. Alle werkplekken zijn gratis en lopen uiteen van cafés tot bibliotheken. Ideaal als je een keer buiten kantoor wilt werken of een keer ergens anders wilt studeren dan thuis."
 
-    title = "Werkplekwijzer - Vind de beste werkplek bij jou in de buurt"
+    title = "Werkplekwijzer - Vind eenvoudig de beste werkplek bij jou in de buurt"
     meta_description = "Een goede werkplek vinden buiten kantoor kan een uitdaging zijn. Werkplekwijzer beoordeelt werkplekken in Nederland op verschillende criteria zodat jij de beste werkplek kunt vinden."
     meta_keywords = "werkplek, werkplekwijzer, flexwerken, werkplekken, rotterdam, amsterdam, den haag, delft"
     meta_viewport = "width=960px"
@@ -85,8 +85,8 @@ def den_haag_filter():
     explanation_title = "Beste werkplekken in Den Haag"
     explanation_body = "Werkplekwijzer zet de beste werkplekken in Den Haag voor je op een rijtje. De werkplekken zijn beoordeeld op verschillende criteria zodat jij gemakkelijk de beste werkplek kunt vinden. Den Haag is de hoofdstad van de provincie Zuid-Holland en is erg goed bereikbaar. De Nederlandse regering en het parlement bevinden zich in Den Haag. De stad is toeristisch en heeft twee bekende stranden: Scheveningen en Kijkduin. Daarnaast zijn er meerdere multinationals gevestigd in Den Haag zoals: Ahold, AEGON, ING Bank en KPN. Aan cafés met goede koffie is in Den Haag geen gebrek. Wij hebben voor jou de beste werkplekken in Den Haag uitgezocht."
 
-    title = "Werkplekwijzer - Beste werkplekken in Den Haag"
-    meta_description = "Werkplekwijzer zet de beste werkplekken in Den Haag voor je op een rijtje. De werkplekken zijn beoordeeld op verschillende criteria zodat jij de beste werkplek kunt vinden."
+    title = "Werkplekwijzer - Vind eenvoudig de beste werkplekken in Den Haag"
+    meta_description = "Werkplekwijzer zet de beste werkplekken in Den Haag voor je op een rijtje. De werkplekken zijn beoordeeld op verschillende criteria voor de beste werkplek."
     meta_keywords = "werkplek, werkplekwijzer, flexwerken, werkplekken, rotterdam, amsterdam, den haag, delft"
     meta_viewport = "width=960px"
     return render_template("home.html", title=title, meta_description=meta_description, meta_keywords=meta_keywords,
@@ -163,8 +163,8 @@ def rotterdam_filter():
     explanation_title = "Beste werkplekken in Rotterdam"
     explanation_body = "Werkplekwijzer heeft de beste werkplekken voor jou uitgezocht. We hebben de werkplekken in Rotterdam beoordeeld op belangrijke criteria zodat je eenvoudig de beste werkplekken kunt vinden. De stad Rotterdam heeft na Amsterdam het meeste aantal inwoners van Nederland. Rotterdam is bekend als havenstad en heeft de grootste haven van Europa. De Rotterdamse haven is een belangrijk logistiek en economisch centrum. Rotterdam is goed bereikbaar en heeft verschillende metrostations. Er zijn een hoop grote bedrijven te vinden zoals: Unilever, Nationale Nederlanden en Robeco. Ook aan werkplekken in Rotterdam zeker geen gebrek. Er zijn veel restaurants en cafés met lekkere koffie waar je wat kunt werken of studeren."
 
-    title = "Werkplekwijzer - Beste werkplekken in Rotterdam"
-    meta_description = "Werkplekwijzer heeft de beste werkplekken voor jou uitgezocht. We hebben de werkplekken in Rotterdam beoordeeld op belangrijke criteria zodat je eenvoudig de beste werkplekken kunt vinden."
+    title = "Werkplekwijzer - Vind eenvoudig de beste werkplekken in Rotterdam"
+    meta_description = "Werkplekwijzer heeft de beste werkplekken in Rotterdam uitgezocht en beoordeeld op belangrijke criteria zodat jij eenvoudig de beste werkplek kunt vinden."
     meta_keywords = "werkplek, werkplekwijzer, flexwerken, werkplekken, rotterdam, amsterdam, den haag, delft"
     meta_viewport = "width=960px"
     return render_template("home.html", title=title, meta_description=meta_description, meta_keywords=meta_keywords,
